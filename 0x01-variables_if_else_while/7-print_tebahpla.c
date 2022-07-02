b382cdf3c5ffcb1629b187d5beba152017f1820b
Derrick Enam Azameti <azametid@gmail.com>
2022-07-01 20:51:11 -0700

    7

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+/**
+ * main - main entry
+ * Description: prints all single digit numbers of base 10
+ * Return: 0
+ */
+int main(void)
+{
+	char l = 'z';
+
+	while (l <= 'z')
+	{
+		putchar(l);
+		l--;
+	}
+
+	putchar('\n');
+	return (0);
+}
