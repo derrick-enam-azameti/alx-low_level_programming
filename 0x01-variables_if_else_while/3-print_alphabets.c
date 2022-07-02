56700f9f5a63cc6cbf0e77d8538379874bcbd2dc
Derrick Enam Azameti <azametid@gmail.com>
2022-07-01 20:13:51 -0700

    3

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+ * main - main entry
+ * Description: prints lower case alphabets then uppercase
+ * Return: 0
+ */
+int main(void)
+{
+	char letter = 'a';
+
+	while (letter <= 'z')
+	{
+		putchar(letter);
+		letter++;
+	}
+
+	letter = 'A';
+
+	while (letter <= 'Z')
+	{
+		putchar(letter);
+		letter++;
+	}
+
+	putchar('\n');
+	return (0);
+}
