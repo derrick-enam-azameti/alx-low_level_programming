15e1b3a1a6e8787e9ea50fff47c1bd2a66c1b7fc
Derrick Enam Azameti <azametid@gmail.com>
2022-07-06 16:40:27 -0700

    104

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#define LARGEST 10000000000
 /**
  * main - main block
  * Description: prints the sum of multiples of 3 or 5
@@ -6,20 +7,31 @@
  */
 int main(void)
 {
-	int i = 0;
-	unsigned long int a = 0, b = 1, next = 0;
+	unsigned long int fb1 = 0, bn1= 1, fb2 = 0, bn2 = 2;
+	unsigned long int h1, h2, h3;
+	int count;
 
-	while (i < 98)
+	printf("%lu, %lu, ", bn1, bn2);
+	for (count = 2; count < 98; count++)
 	{
-		next = a + b;
-		a = b;
-		b = next;
-		printf("%lu", next);
-
-		if (i < 97)
+		if (bn1 + bn2 > LARGEST || fb2 > 0 || fb1 > 0)
+		{
+			h1 = (bn1 +bn2) / LARGEST;
+			h2 = (bn1 + bn2) % LARGEST;
+			h3 = fb1 + fb2 + h1;
+			fb1 = fb2, fb2 = h3;
+			bn1 = bn2, bn2 = h2;
+			printf("%lu%010lu", fb2, bn2);
+		}
+		else
+		{
+			h2 = bn1 + bn2;
+			bn1 = bn2, bn2 = h2;
+			printf("%lu", bn2);
+		}
+		if (count != 97)
 			printf(", ");
-		i++;
 	}
-	putchar('\n');
+	printf("\n");
 	return (0);
 }
