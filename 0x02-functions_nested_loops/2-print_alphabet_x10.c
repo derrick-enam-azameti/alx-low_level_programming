4fa9755a93e185eaccd823ba702c62ffd503bed1
Derrick Enam Azameti <azametid@gmail.com>
2022-07-06 14:22:29 -0700

    2

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - prints the alphabet 10 times
+ * Return: void
+ */
+void print_alphabet_x10(void)
+{
+	char x;
+	char y;
+
+	for (y = 0; y < 10; y++)
+	{
+		for (x = 'a'; x <= 'z'; x++)
+		{
+			_putchar(x);
+		}
+		_putchar('\n');
+	}
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -13,5 +13,6 @@ int _abs(int n);
 int print_sign(int n);
 int _isalpha(int c);
 int _islower(int c);
+void print_alphabet_x10(void);
 
 #endif /* MAIN_H */
