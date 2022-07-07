74beebe2bad62825f9389d8b930dd3ed7d8857f0
Derrick Enam Azameti <azametid@gmail.com>
2022-07-07 15:53:18 -0700

    ---15

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * print_diagonal - draws diagonal line
+ * @n: int
+ * Return: 0
+ */
+void print_diagonal(int n)
+{
+	int i = 0, j;
+
+	while (i < n && n > 0)
+	{
+		j = 0;
+		while (j < i)
+		{
+			_putchar(' ');
+			j++;
+		}
+
+		_putchar('\\');
+		_putchar('\n');
+		i++;
+	}
+	if (i == 0)
+		_putchar('\n');
+}
