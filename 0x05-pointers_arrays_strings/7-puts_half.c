913cd6739a7938569f878303f55bfe108f74af5c
Derrick Enam Azameti <azametid@gmail.com>
2022-07-11 09:35:46 -0700

    8

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ * puts_half - prints half of a string
+ * @str: pointer to string
+ * Return: void
+ */
+
+void puts_half(char *str)
+{
+	int len = 0, i, n;
+
+	while (str[len] != '\0')
+		len++;
+
+	if (len % 2 == 0)
+	{
+		n = len / 2;
+	}
+	else
+	{
+		n = (lne + 1) / 2;
+	}
+
+	for (i = n; i < len; i++)
+		_putchar(str[i]);
+
+	_putchar('\n');
+}
