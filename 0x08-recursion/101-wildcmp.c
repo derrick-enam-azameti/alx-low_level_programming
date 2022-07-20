473d78e8059ec76dca53be071fbab53f9b16e2c0
Derrick Enam Azameti <azametid@gmail.com>
2022-07-20 10:52:36 -0700

    101-wildcmp.c

---
+++ 0x08-recursion/101-wildcmp.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * wildcmp - compares two strings to see if they are identical
+ * @s1: first string
+ * @s2: second string
+ * Return: 1 or 0
+ */
+int wildcmp(char *s1, char *s2)
+{
+	if (!*s1 && !*s2)
+		return (1);
+	if (*s1 == *s2)
+		return (wildcmp(s1 + 1, s2 + 1));
+	if (*s2 == '*' && (wildcmp(s1, s2 + 1) || wildcmp(s1 + 1, s2)))
+		return (1);
+	if (*s2 == '*' && *(s1 + 1) && *s2)
+		return (0);
+	return (0);
+}
