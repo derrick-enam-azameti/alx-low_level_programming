3c5f629b229da2b3b5cfdf094073372a8d6f32eb
Derrick Enam Azameti <azametid@gmail.com>
2022-07-25 17:58:45 -0700

    3-alloc_grid.c

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -10,7 +10,7 @@ int **alloc_grid(int width, int height)
 	int **tab, i, j;
 
 	tab = malloc(sizeof(*tab) * height);
-	if (widht <= 0 || height <= 0 || tab == 0)
+	if (width <= 0 || height <= 0 || tab == 0)
 	{
 		return (NULL);
 	}
