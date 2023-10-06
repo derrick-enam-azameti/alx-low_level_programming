6845202e5e227958cda92202fa57d3fd6c2e6707
Derrick Enam Azameti <azametid@gmail.com>
2023-10-06 15:08:40 -0700

    add 5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: print all single digit numbers
+ * of base 10 starting from 0, followed by a
+ * new line.
+ *
+ * Return: 0 (Success)
+ */
+
+int main(void)
+{
+	int num;
+
+	for (num = 0; num < 10; num++)
+		printf("%d", num);
+	printf("\n");
+
+	return (0);
+}
