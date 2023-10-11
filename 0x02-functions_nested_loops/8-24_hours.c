27d8119a644ce34e34438491d37e8bc6895cbfdb
Derrick Enam Azameti <azametid@gmail.com>
2023-10-10 17:05:47 -0700

    add 8-24_hours.c

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * jack_bauer - prints every minute of the day of Jack Bauer
+ * Description: starts from 00:00 to 23:59
+ */
+
+void jack_bauer(void)
+{
+	int h, m;
+
+	for (h = 0; h < 24; h++)
+	{
+		int m = 0;
+
+		while (m < 60)
+		{
+			_putchar('0' + h / 10);
+			_putchar('0' + h % 10);
+			_putchar(':');
+			_putchar('0' + m / 10);
+			_putchar('0' + m % 10);
+			_putchar('\n');
+			m++;
+		}
+	}
+}
