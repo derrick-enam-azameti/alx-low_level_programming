6542fd76a49e4bddad767808ed0ae06646154b88
Derrick Enam Azameti <azametid@gmail.com>
2023-10-19 16:20:19 -0700

    add 3-strcmp.c

---
+++ 0x06-pointers_arrays_strings/4-main.c
@@ -0,0 +1,31 @@
+Write a function that reverses the content of an array of integers.
+
+Prototype: void reverse_array(int *a, int n);
+Where n is the number of elements of the array
+julien@ubuntu:~/0x06$ cat 4-main.c
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - check the code
+ * @a: an array of integers
+ * @n: the number of elements to swap
+ *
+ * Return: nothing.
+ */
+void print_array(int *a, int n)
+{
+    int i;
+
+    i = 0;
+    while (i < n)
+    {
+        if (i != 0)
+        {
+            printf(", ");
+        }
+        printf("%d", a[i]);
+        i++;
+    }
+    printf("\n");
+}
